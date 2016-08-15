fe11fd51282ee78f6e6b01c9767916a48bd7829a
shaelarie <shae.larie@gmail.com>
2016-08-15 10:47:42 -0500

    adding project files

---
+++ PaulKonerkoAverages/Averages.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaulKonerkoAverages
+{
+    class Averages
+    {
+        
+
+
+
+
+
+        public void RunLists()
+        {
+
+            var statInfo = new[] { "Games: ", "At Bats: ", "Runs: ", "Hits: " };
+            var statType = new[] { 81, 208, 15, 43 };
+            
+
+            
+
+            foreach (var TI in statType.Zip(statInfo, Tuple.Create))
+            {
+                Console.WriteLine(TI.Item2 + TI.Item1);
+            }
+
+
+
+        }
+
+
+    }
+}
---
+++ PaulKonerkoAverages/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaulKonerkoAverages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Averages averages = new Averages();
+            averages.RunLists();
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
